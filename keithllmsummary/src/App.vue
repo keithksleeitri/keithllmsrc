446c3da38428bc554623bc4ac374dace2944175e
keithksleeitri <53255355+keithksleeitri@users.noreply.github.com>
2024-05-21 15:46:56 +0800

    Bonus VUE frontend

---
+++ keithllmsummary/src/App.vue
@@ -0,0 +1,124 @@
+<template>
+    <div id="app">
+        <h4>ChatGPT 對話視窗</h4>
+        <TalkList
+            ref="chat-container"
+            :talk-contents="talk_contents"
+        />
+        <WordsInput
+            v-model="words"
+            @send="send"
+            :disabled="is_wait_chatgpt_response === true"
+        />
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import TalkList from './components/talk-list.vue';
+import WordsInput from './components/words-input.vue';
+
+export default {
+    name: 'App',
+    data: () => ({
+        // 對話內容
+        talk_contents: [],
+        // 輸入對話內容
+        words: '',
+        // 等待 ChatGPT
+        is_wait_chatgpt_response: false,
+        // ChatGPT Setting
+        api_settings: {
+            //url: 'https://api.openai.com/v1/chat/completions',
+            url: 'http://127.0.0.1:3000/chatgpt/',
+            model: 'gpt-3.5-turbo-0301',
+            api_token: 'sk-proj-5uSlpfb2X0bFymtKu1IfT3BlbkFJvaBrVBB39N0i2CncBTe9',
+            organization_id: 'org-ibB2LsNwrTtXfhGZ6BswtvtB',
+        },
+    }),
+    methods: {
+        async sendRequestToChatGPT(words) {
+
+            //const headers = {
+            //    "Content-Type": "application/json",
+            //    "Authorization": `Bearer ${this.api_settings.api_token}`,
+            //    "OpenAI-Organization": this.api_settings.organization_id,
+            //}
+
+            //const params = {
+            //    model: 'gpt-3.5-turbo-0301',
+            //    messages: [
+            //        {
+            //            role: 'user',
+            //            content: words
+            //        }
+            //    ],
+            //    temperature: 0.7
+            //}
+            
+            //const response = await axios.post(this.api_settings.url, params, {
+            //    headers: headers,
+            //});
+
+            const response = await axios.get(this.api_settings.url+words)
+
+            let result = {};
+            if(response.status === 200) {
+                result = await response.data;
+            }
+
+            return result;
+        },
+        // 送出
+        async send() {
+
+            // 送出後須鎖定送出按鈕(避免重覆發送)
+            this.is_wait_chatgpt_response = true;
+
+            // 對話櫃加入自己的發話
+            let words = this.words;
+            this.talk_contents.push({
+                class: 'user',
+                user: '我',
+                content: words,
+            });
+            // 清除發話內容
+            this.words = '';
+
+            // 發送請求給 ChatGPT API
+            const response = await this.sendRequestToChatGPT(words);
+
+            // 取得 API 回應後，解析回應內容並取出回應訊息
+            let message = '';
+            if(response.choices !== undefined && response.choices.length > 0) {
+                //message = response.choices[0].message.content;
+                message = response.summary
+            }
+            this.talk_contents.push({
+                class: 'chatgpt',
+                user: 'ChatGPT',
+                content: message,
+            });
+
+            this.$nextTick(function() {
+                this.is_wait_chatgpt_response = false;
+                // 自動滑到底
+                let height = this.$refs['chat-container'].clientHeight;
+                let scrollHeight = this.$refs['chat-container'].scrollHeight;
+                let scrollTop = scrollHeight - height + 100;
+                this.$refs['chat-container'].scrollTop = scrollTop;
+            })
+        },
+    },
+    components: {
+        WordsInput,
+        TalkList,
+    },
+}
+</script>
+
+<style scoped>
+#app {
+    padding: 1rem;
+}
+</style>
---
+++ keithllmsummary/src/components/talk-list.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="chat-container">
+        <div
+            class="chat-item"
+            v-for="(item, index) in talkContents"
+            :key="index"
+            :class="item.class"
+        >
+            <div class="person">{{ item.user }}: </div>
+            <div class="content">{{ item.content }} </div>
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+module.exports = {
+    // ---- (設置元件名稱) ----
+    name: 'talk-list',
+    props: {
+        // v-model用屬性
+        talkContents: {
+            type: Array,
+            default: [],
+        },
+    },
+}
+</script>
+
+<style scoped>
+.chat-container {
+    width: 100%;
+    height: 75vh;
+    border: 1px #333 solid;
+    border-radius: 1rem;
+    margin-bottom: 1rem;
+    overflow-y: scroll;
+    display: block;
+}
+.chat-container .chat-item {
+    width: 100%;
+    display: flex;
+    margin-bottom: 0.25rem;
+}
+
+.chat-container .chat-item.user {
+    background: #BBB;
+    padding: 0.5rem 0.25rem;
+}
+
+.chat-container .chat-item.chatgpt {
+    background: #DDD;
+    padding: 0.5rem 0.25rem;
+}
+
+.person {
+    width: 5rem;
+    text-align: right;
+}
+.content {
+    width: calc(100% - 5rem);
+    padding-left: 0.75rem;
+}
+</style>
